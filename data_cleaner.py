0f56c62bf8b9097c5666f0f831109a68edb8aadc
lisasilisa <lisasoboth4@gmail.com>
2022-05-18 23:46:46 +0200

    updated and optimized data_cleaner code

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,38 +1,35 @@
+import pandas as pd
 from datetime import date
 from datetime import datetime
 
 
-def data_cleaner(dataset, period_start_date:str, period_end_date:str, **slice_params):
+def data_cleaner(dataset, account:str, period_start_date:str, period_end_date:str):
     """
     Ausführung der einzelnen Schritte, um Datensatz zu bereinigen
     """
-    subset = get_rel_cols(dataset)   
+    subset = get_rel_cols(dataset) 
     subset = update_to_consistent_cols_names(subset)
     subset = add_start_and_end_date_cols(subset)
     subset = clean_state_col(subset)
     subset = get_rel_time_data(subset, period_start_date, period_end_date)
-    subset = get_rel_data(subset, **slice_params)
+    subset = get_account_data(subset, account)
     return subset
-    
+
 
 def get_rel_cols(dataset):
     """
     relevante Spalten des Datensatzs definieren
     """
     dataset = dataset[['Account', 'User', 'Partition', 'Start', 'End', 'CPUTime', 'CPUTimeRAW', 'Elapsed',  
-                    'ElapsedRaw',  'AllocCPUS', 'Timelimit', 'TimelimitRaw', 'State']]
+                    'ElapsedRaw',  'AllocCPUS', 'State']]
     return dataset
 
 
-def get_rel_data(dataset, **slice_params):
+def get_account_data(dataset, account):
     """
-    Datensatz auf bestimmte Einträge in Spalten slicen;
-    testet auf Gleichheit
-    z.B. User, Account
+    Datenset nach Arbeitsgruppe vorslicen
     """
-    print('slice_params', slice_params)
-    for param, value in slice_params.items():
-        dataset = dataset[dataset[param] == value]
+    dataset = dataset[dataset['Account'] == account]
     return dataset
 
 
@@ -49,13 +46,12 @@ def add_start_and_end_date_cols(dataset):
     Spalten hinzufügen, welche Start und Endtag als datetime Objekte enthalten
     """
     start_date = dataset['Start'].str.split('T').str[0]  
-    start_date_datetime = start_date.apply(lambda x: datetime.strptime(x, "%Y-%m-%d").date())
-    #start_day_datetime = datetime.strptime(start_day, "%Y-%m-%d").date()
+
+    start_date_datetime = pd.to_datetime(start_date)
     dataset.insert(4, 'StartDate', start_date_datetime)
     
     end_date = dataset['End'].str.split('T').str[0]  
-    end_date_datetime = end_date.apply(lambda x: datetime.strptime(x, "%Y-%m-%d").date())
-    #end_day_datetime = datetime.strptime(end_day, "%Y-%m-%d").date()
+    end_date_datetime = pd.to_datetime(end_date)
     dataset.insert(6, 'EndDate', end_date_datetime)
     
     return dataset
@@ -63,11 +59,16 @@ def add_start_and_end_date_cols(dataset):
 
 def get_rel_time_data(dataset, period_start_date:str, period_end_date:str):
     """ 
-    Datensatz auf Einträge slicen, welche innerhalb des Zeitraums angefangen und beendet wurden
+    Datensatz auf Einträge slicen, welche innerhalb des Zeitraums angefangen oder beendet wurden
     """
-    period_start_date_dt = datetime.strptime(period_start_date, "%Y-%m-%d").date() # dt steht für datetime Objekt
-    period_end_date_dt = datetime.strptime(period_end_date, "%Y-%m-%d").date()
-    dataset = dataset[dataset['StartDate'] >= period_start_date_dt][dataset['EndDate'] <= period_end_date_dt]
+    period_start_date_dt = pd.to_datetime(period_start_date)
+    period_end_date_dt = pd.to_datetime(period_end_date)
+
+    subset = dataset[(dataset['StartDate'] < period_start_date_dt) | (dataset['StartDate'] > period_end_date_dt)] # Aufträge die nicht in Zeitspanne starten
+    subset = subset[(subset['EndDate'] < period_start_date_dt) | (subset['EndDate'] > period_end_date_dt)] # Aufträge die zusätzlich auch nicht in Zeitspanne enden
+    dataset = dataset[~dataset.index.isin(subset.index)] # alle Aufträge außer denen die weder in Zeitspanne anfangen oder enden 
+
+
     return dataset
 
 

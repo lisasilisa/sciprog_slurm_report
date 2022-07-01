ce0ccd8834aa18b84030af82dd77561196479d45
maxhilsdorf <maxhilsdorf@web.de>
2022-07-01 12:16:52 +0200

    Doc builder now makes user/partition split only when count >= 2

--- document_builder.py
+++ document_builder.py
@@ -93,51 +93,53 @@ def build_document(df, stats_dict, viz_path="fig/", doc_name="slurm_report"):
                 fig.add_image("fig/basic_stats_full.jpg", width="250px")
                 fig.add_caption("Number of started and ended tasks.")
 
-        with doc.create(Subsection("User Split")):
-            
-            user_names = stats_dict["user_split"]["user_names"]
-            user_counts = stats_dict["user_split"]["user_counts"]
-            n_over_ten_jobs = sum([1 for x in user_counts if x >=10])
-            data = stats_dict["user_split"]["basic_stats"]
+        if "user_split" in stats_dict.keys():
+            with doc.create(Subsection("User Split")):
+                
+                user_names = stats_dict["user_split"]["user_names"]
+                user_counts = stats_dict["user_split"]["user_counts"]
+                n_over_ten_jobs = sum([1 for x in user_counts if x >=10])
+                data = stats_dict["user_split"]["basic_stats"]
 
-            doc.append(
-                f"The number of tasks (started and ended) ranged from \
-                {data['n_start_end'][0]} by {user_names[0]} to \
-                {data['n_start_end'][-1]} by {user_names[-1]}. "
-            )
+                doc.append(
+                    f"The number of tasks (started and ended) ranged from \
+                    {data['n_start_end'][0]} by {user_names[0]} to \
+                    {data['n_start_end'][-1]} by {user_names[-1]}. "
+                )
 
-            doc.append(
-                f"A total of {n_over_ten_jobs} user(s) started and ended at least 10 tasks. \
-                Only for those users, boxplots are computed in Section 3."
-            )
+                doc.append(
+                    f"A total of {n_over_ten_jobs} user(s) started and ended at least 10 tasks. \
+                    Only for those users, boxplots are computed in Section 3."
+                )
 
-            with doc.create(Figure(position="h!")) as fig:
-                fig.add_image("fig/basic_stats_user_split.jpg", width="275px")
-                fig.add_caption("Number of started and ended tasks by different users.")  
+                with doc.create(Figure(position="h!")) as fig:
+                    fig.add_image("fig/basic_stats_user_split.jpg", width="275px")
+                    fig.add_caption("Number of started and ended tasks by different users.")  
 
-        with doc.create(Subsection("Partition Split")):
+        if "partition_split" in stats_dict.keys():
+            with doc.create(Subsection("Partition Split")):
 
-            partition_names = stats_dict["partition_split"]["partition_names"]
-            partition_counts = stats_dict["partition_split"]["partition_counts"]
-            n_over_ten_jobs = sum([1 for x in partition_counts if x >=10])
-            data = stats_dict["partition_split"]["basic_stats"]
+                partition_names = stats_dict["partition_split"]["partition_names"]
+                partition_counts = stats_dict["partition_split"]["partition_counts"]
+                n_over_ten_jobs = sum([1 for x in partition_counts if x >=10])
+                data = stats_dict["partition_split"]["basic_stats"]
 
-            doc.append(
-                f"The number of tasks (started and ended) ranged from \
-                {data['n_start_end'][0]} on the {partition_names[0]} partiton to \
-                {data['n_start_end'][-1]} on {partition_names[-1]}. "
-            )
+                doc.append(
+                    f"The number of tasks (started and ended) ranged from \
+                    {data['n_start_end'][0]} on the {partition_names[0]} partiton to \
+                    {data['n_start_end'][-1]} on {partition_names[-1]}. "
+                )
 
-            doc.append(
-                f"A total of {n_over_ten_jobs} partition(s) were used to complete (start and end) \
-                at least 10 tasks. Only for those partitions, boxplots are computed in Section 3."
-            )
+                doc.append(
+                    f"A total of {n_over_ten_jobs} partition(s) were used to complete (start and end) \
+                    at least 10 tasks. Only for those partitions, boxplots are computed in Section 3."
+                )
 
-            data = stats_dict["partition_split"]["basic_stats"]
+                data = stats_dict["partition_split"]["basic_stats"]
 
-            with doc.create(Figure(position="h!")) as fig:
-                fig.add_image("fig/basic_stats_partition_split.jpg", width="275px")
-                fig.add_caption("Number of started and ended tasks on different partitions.")  
+                with doc.create(Figure(position="h!")) as fig:
+                    fig.add_image("fig/basic_stats_partition_split.jpg", width="275px")
+                    fig.add_caption("Number of started and ended tasks on different partitions.")  
 
     ## TASK METRICS ##
 
@@ -161,7 +163,7 @@ def build_document(df, stats_dict, viz_path="fig/", doc_name="slurm_report"):
             )
 
             doc.append(
-                f"Finally, the CPU Time (job duration * allocated CPUs) ranged from \
+                f"Finally, the CPU Time (task duration * allocated CPUs) ranged from \
                 {round(data['CPUTimeRaw'][0]/60)} to {round(data['CPUTimeRaw'][-2]/60)} minutes \
                 (M = {round(data['CPUTimeRaw'][-1]/60)} min). \n"
             )
@@ -185,70 +187,72 @@ def build_document(df, stats_dict, viz_path="fig/", doc_name="slurm_report"):
             with doc.create(Figure(position="h!")) as fig:
                 fig.add_image("fig/task_metrics_full.jpg", width="400px")
                 fig.add_caption("Distributions of task duration, allocated CPUs, and CPU time. Whiskers indicate the 5th and 95th percentiles.")  
-
+        
         doc.append(NoEscape(r"\pagebreak"))
-        with doc.create(Subsection("User Split")):
+        if "user_split" in stats_dict.keys():
+            with doc.create(Subsection("User Split")):
 
-            data = stats_dict["user_split"]["task_metrics"]
+                data = stats_dict["user_split"]["task_metrics"]
 
-            # Some text?
-            doc.append("Below, you can find the distributions of the task metrics for each user. In the table, all users are listed. Boxplots are only generated for users with at least 10 jobs.\n")
+                # Some text?
+                doc.append("Below, you can find the distributions of the task metrics for each user. In the table, all users are listed. Boxplots are only generated for users with at least 10 jobs.\n")
 
-            # Prepare data for table
-            data_array = np.zeros((len(user_names),4), dtype=tuple)
-            data_array[:,0] = user_counts
-            for user_idx in range(len(user_names)):
-                for metric_idx, metric in enumerate(data.keys()):
-                    metric_vals = data[metric][user_idx]
-                    if metric_idx == 0:
-                        data_array[user_idx,metric_idx+1] = (round(metric_vals[0]), round(metric_vals[-1]), round(metric_vals[-2]))
-                    else:
-                        data_array[user_idx,metric_idx+1] = (round(metric_vals[0])/60, round(metric_vals[-1]/60), round(metric_vals[-2]/60))
+                # Prepare data for table
+                data_array = np.zeros((len(user_names),4), dtype=tuple)
+                data_array[:,0] = user_counts
+                for user_idx in range(len(user_names)):
+                    for metric_idx, metric in enumerate(data.keys()):
+                        metric_vals = data[metric][user_idx]
+                        if metric_idx == 0:
+                            data_array[user_idx,metric_idx+1] = (round(metric_vals[0]), round(metric_vals[-1]), round(metric_vals[-2]))
+                        else:
+                            data_array[user_idx,metric_idx+1] = (round(metric_vals[0])/60, round(metric_vals[-1]/60), round(metric_vals[-2]/60))
 
-            #print(data_array)
-            
-            # Build table
-            with doc.create(Table(position="h!")) as t:
-                build_table(doc=doc, data=data_array, col_names=["Num Jobs", "Allocated CPUs", "Task Duration", "CPU Time"],
-                            position_codes="l c c c", index=user_names)
-                t.add_caption("Task metrics for different users. Values in parantheses indicate (min, mean, max). Time is measured in seconds.")
+                #print(data_array)
+                
+                # Build table
+                with doc.create(Table(position="h!")) as t:
+                    build_table(doc=doc, data=data_array, col_names=["Num Jobs", "Allocated CPUs", "Task Duration", "CPU Time"],
+                                position_codes="l c c c", index=user_names)
+                    t.add_caption("Task metrics for different users. Values in parantheses indicate (min, mean, max). Time is measured in seconds.")
 
-            # Show plot
-            with doc.create(Figure(position="h!")) as fig:
-                fig.add_image("fig/task_metrics_user_split.jpg", width="350px")
-                fig.add_caption("Distributions of job duration, allocated CPUs, and CPU time for different users. Whiskers indicate the 5th and 95th percentiles.") 
+                # Show plot
+                with doc.create(Figure(position="h!")) as fig:
+                    fig.add_image("fig/task_metrics_user_split.jpg", width="350px")
+                    fig.add_caption("Distributions of job duration, allocated CPUs, and CPU time for different users. Whiskers indicate the 5th and 95th percentiles.") 
 
-        doc.append(NoEscape(r"\pagebreak"))
-        with doc.create(Subsection("Partition Split")):
+            doc.append(NoEscape(r"\pagebreak"))
+        if "partition_split" in stats_dict.keys():
+            with doc.create(Subsection("Partition Split")):
 
-            data = stats_dict["partition_split"]["task_metrics"]
+                data = stats_dict["partition_split"]["task_metrics"]
 
-            # Some text?
-            doc.append("Below, you can find the distributions of the task metrics for each partition. In the table, all partitions are listed. Boxplots are only generated for partitions with at least 10 jobs.\n")
+                # Some text?
+                doc.append("Below, you can find the distributions of the task metrics for each partition. In the table, all partitions are listed. Boxplots are only generated for partitions with at least 10 jobs.\n")
 
-            # Prepare data for table
-            data_array = np.zeros((len(partition_names),4), dtype=tuple)
-            data_array[:,0] = partition_counts
-            for partition_idx in range(len(partition_names)):
-                for metric_idx, metric in enumerate(data.keys()):
-                    metric_vals = data[metric][partition_idx]
-                    if metric_idx == 0:
-                        data_array[partition_idx,metric_idx+1] = (round(metric_vals[0]), round(metric_vals[-1]), round(metric_vals[-2]))
-                    else:
-                        data_array[partition_idx,metric_idx+1] = (round(metric_vals[0]/60), round(metric_vals[-1]/60), round(metric_vals[-2]/60))
+                # Prepare data for table
+                data_array = np.zeros((len(partition_names),4), dtype=tuple)
+                data_array[:,0] = partition_counts
+                for partition_idx in range(len(partition_names)):
+                    for metric_idx, metric in enumerate(data.keys()):
+                        metric_vals = data[metric][partition_idx]
+                        if metric_idx == 0:
+                            data_array[partition_idx,metric_idx+1] = (round(metric_vals[0]), round(metric_vals[-1]), round(metric_vals[-2]))
+                        else:
+                            data_array[partition_idx,metric_idx+1] = (round(metric_vals[0]/60), round(metric_vals[-1]/60), round(metric_vals[-2]/60))
 
-            #print(data_array)
-            
-            # Build table
-            with doc.create(Table(position="h!")) as t:
-                build_table(doc=doc, data=data_array, col_names=["Num Jobs", "Allocated CPUs", "Task Duration", "CPU Time"],
-                            position_codes="l c c c", index=partition_names)
-                t.add_caption("Task metrics for different partitions. Values in parantheses indicate (min, mean, max). Time is measured in seconds.")
+                #print(data_array)
+                
+                # Build table
+                with doc.create(Table(position="h!")) as t:
+                    build_table(doc=doc, data=data_array, col_names=["Num Jobs", "Allocated CPUs", "Task Duration", "CPU Time"],
+                                position_codes="l c c c", index=partition_names)
+                    t.add_caption("Task metrics for different partitions. Values in parantheses indicate (min, mean, max). Time is measured in seconds.")
 
-            # Show plot
-            with doc.create(Figure(position="h!")) as fig:
-                fig.add_image("fig/task_metrics_partition_split.jpg", width="350px")
-                fig.add_caption("Distributions of job duration, allocated CPUs, and CPU time for different partitions. Whiskers indicate the 5th and 95th percentiles.") 
+                # Show plot
+                with doc.create(Figure(position="h!")) as fig:
+                    fig.add_image("fig/task_metrics_partition_split.jpg", width="350px")
+                    fig.add_caption("Distributions of job duration, allocated CPUs, and CPU time for different partitions. Whiskers indicate the 5th and 95th percentiles.") 
 
 
     ## TERMINATION STATS ##

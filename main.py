c951c55e2f69a8f189b9912e8601cbca827c2f46
maxhilsdorf <maxhilsdorf@web.de>
2022-07-03 21:04:49 +0200

    Implemented error message for empty dataframe cases

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+Notes:
+- make sure visualization path exists (i.e. fig/)
+"""
+
 from data_cleaner import data_cleaner
 from stats_extractor import StatsExtractor
 from data_visualizer import DataVisualizer
@@ -8,7 +13,7 @@ import pandas as pd
 import numpy as np
 
 
-DATASET_PATH = "E:/OneDrive/uni_work/sose22/sci_prog/dataset/slurmaccountdata/slurmaccountdata_shortened.csv"
+DATASET_PATH = "../dataset/slurmaccountdata/slurmaccountdata_shortened.csv"
 ACCOUNT_NAME = "59628c0f-aa89-4a69"
 START_DATE = "2020-02-01"
 END_DATE = "2021-09-30"
@@ -22,9 +27,14 @@ def main():
     cleaned_dataset = data_cleaner(dataset, account=ACCOUNT_NAME, period_start_date =START_DATE, period_end_date=END_DATE) # Beispieldaten
     #cleaned_dataset.to_csv("dev_df.csv", index=False)
 
+    if len(cleaned_dataset) == 0:
+        print("No tasks for this account were recorded in the given time frame.")
+        return
+
     print("... extracting stats ... (3/5)")
     S = StatsExtractor(cleaned_dataset)
     stats_dict = S.extract_stats()
+    #print(stats_dict)
 
     print("... creating visualizations ... (4/5)")
     Viz = DataVisualizer(stats_dict, set_plot_config())
